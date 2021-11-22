8f7b00d65932fcaf576cfd9b1ed930464c5f457d
Tom Meadon <tom@meadon.me>
2021-11-22 17:38:21 +0000

    add k6 test

---
+++ tests/k6.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+    checks: ['rate>0.95']
+  },
+  noConnectionReuse: true
+};
+
+export default function () {
+  const params = {
+    timeout: 2000
+  }
+  const url = `http://${__ENV.HOST}`
+  const res = http.get(url, params);
+  check(res, {
+    'status is 200': (res) => res.status === 200,
+  });
+  sleep(1);
+}
