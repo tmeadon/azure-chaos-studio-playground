4b1e7b07748bbc349c055618cdb9a4dfd8e78394
Tom Meadon <tom@meadon.me>
2021-11-23 12:03:55 +0000

    run chaos experiement in pipeline

---
+++ scripts/start-experiment.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+RESOURCE_GROUP=$1
+EXPERIMENT_NAME=$2
+
+# get the resource id for the experiment and construct the start rest api url
+EXPERIMENT_ID=$(az resource show -n $EXPERIMENT_NAME -g $RESOURCE_GROUP --resource-type Microsoft.Chaos/experiments --query id -o tsv)
+START_URL="https://management.azure.com${EXPERIMENT_ID}/start?api-version=2021-09-15-preview"
+
+# start the experiment and capture the status url
+STATUS_URL=$(az rest --method post --uri $START_URL --body '{}' --query statusUrl --output tsv)
+STATUS=""
+
+# wait for the experiement to transition to 'running' 
+while [ "$STATUS" != "Running" ]
+do
+    STATUS=$(az rest --method get --uri $STATUS_URL --query properties.status --output tsv)
+    echo "Experiment status: ${STATUS}"
+    sleep 5
+done
